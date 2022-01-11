0d41574b4e81c88e23aea25a5f8ce11bf0dac19e
regularPark <90092440+regularPark@users.noreply.github.com>
2022-01-11 15:28:35 +0900

    1/11

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-const content = [
-  {
-    tab: "section 1",
-    content: "I'm the content of the Section 1"
-  },
-  {
-    tab: "section 2",
-    content: "I'm the content of the Section 2"
-  }
-];
-const useTabs = (initialTab, allTabs) => {
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex
-  };
-};
-
 const App = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  useEffect(sayHello, [number]);
+
+  const [aNumber, setANumber] = useState(0);
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
