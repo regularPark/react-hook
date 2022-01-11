40fe274169ee8d38cc5ea6628f7532bd509ad557
regularPark <90092440+regularPark@users.noreply.github.com>
2022-01-11 15:14:26 +0900

    useTabs

--- src/index.js
+++ src/index.js
@@ -11,13 +11,25 @@ const content = [
     content: "I'm the content of the Section 2"
   }
 ];
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
 
 const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      {content.map((section) => (
-        <button>{section.tab}</button>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
       ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
